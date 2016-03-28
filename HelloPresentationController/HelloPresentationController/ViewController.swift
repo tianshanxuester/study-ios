9404b39c2797fc059bcbff5f35a75cbfa4eb0c4d
yangzigang <yangzigang@nationsky.com>
2016-03-28 10:38:32 +0800

    routine

---
+++ HelloPresentationController/HelloPresentationController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloPresentationController
+//
+//  Created by 杨子刚 on 15/11/30.
+//  Copyright © 2015年 yangzigang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
